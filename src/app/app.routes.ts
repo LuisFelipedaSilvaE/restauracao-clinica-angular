6eebab7ec18c4eda79c1c485d528bed17db2f025
Luis Felipe <2511550630@fema.edu.br>
2026-08-11 13:20:11 -0300

    feat: implementar estrutura inicial do componente de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('../app/features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+    canActivate: [authGuard],
+  },
+];
