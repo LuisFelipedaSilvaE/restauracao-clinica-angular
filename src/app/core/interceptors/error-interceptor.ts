260dc2a587fba198a4395df18d476a2feb6288b4
Dario Klein <dariokleinalves@gmail.com>
2026-08-21 01:03:58 -0300

    feat(auth): adicionar tratamento para erro HTTP 403 no errorInterceptor

--- src/app/core/interceptors/error-interceptor.ts
+++ src/app/core/interceptors/error-interceptor.ts
@@ -3,22 +3,34 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth-service';
 import { catchError, throwError } from 'rxjs';
+import { MessageService } from 'primeng/api';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  if (req.url.includes('/auth/login')) {
-    return next(req);
-  }
+  // if (req.url.includes('/auth/login')) {
+  //   return next(req);
+  // }
 
   const router = inject(Router);
   const authService = inject(AuthService);
+  const messageService = inject(MessageService);
 
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
-      if (error.status === 401) {
-        authService.removeToken();
-        router.navigate(['/login'], {
-          queryParams: { sessionExpired: true },
-        });
+      switch (error.status) {
+        case 401:
+          authService.removeToken();
+          router.navigate(['/login'], {
+            queryParams: { sessionExpired: true },
+          });
+
+          break;
+        case 403:
+          messageService.add({
+            severity: 'error',
+            summary: 'Acesso negado',
+            detail: 'Você não tem permissão para realizar esta ação.',
+          });
+          break;
       }
       return throwError(() => error);
     }),
