d63866c131fe4ffa07af9fafa2e0f55d26a6d87d
Luis Felipe <2511550630@fema.edu.br>
2026-08-25 18:00:17 -0300

    refactor: remover lazy-loading excessivo nas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth-guard';
 import { roleGuard } from './core/guards/role-guard';
+import { Layout } from './layout/layout';
+import { ModalidadesLista } from './features/modalidades/pages/modalidades-lista/modalidades-lista';
+import { FuncionariosLista } from './features/funcionarios/pages/funcionarios-lista/funcionarios-lista';
+import { NotFound } from './core/pages/not-found/not-found';
 
 export const routes: Routes = [
   {
@@ -9,27 +13,29 @@ export const routes: Routes = [
   },
   {
     path: '',
-    loadComponent: () => import('./layout/layout').then((m) => m.Layout),
+    component: Layout,
     canActivate: [authGuard],
     children: [
       {
         path: 'modalidades',
-        loadComponent: () =>
-          import('./features/modalidades/pages/modalidades-lista/modalidades-lista').then(
-            (m) => m.ModalidadesLista,
-          ),
+        component: ModalidadesLista,
+        canActivate: [roleGuard('ADMIN')],
+      },
+      {
+        path: 'funcionarios',
+        component: FuncionariosLista,
         canActivate: [roleGuard('ADMIN')],
       },
       {
         path: '**',
-        loadComponent: () => import('./core/pages/not-found/not-found').then((m) => m.NotFound),
+        component: NotFound,
         data: { inLayout: true },
       },
     ],
   },
   {
     path: '**',
-    loadComponent: () => import('./core/pages/not-found/not-found').then((m) => m.NotFound),
+    component: NotFound,
     data: { inLayout: false },
   },
 ];
